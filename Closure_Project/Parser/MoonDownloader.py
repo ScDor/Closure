6848b0cb39249d6372bb8300fa2cd620afccd64b
yaelmualem <80756908+yaelmualem@users.noreply.github.com>
2021-06-17 12:15:28 +0300

    Yaelmualem/add retry to download data (#48)
    * skip existing files
    * add retry to data downloader

--- Closure_Project/Parser/MoonDownloader.py
+++ Closure_Project/Parser/MoonDownloader.py
@@ -1,58 +1,69 @@
 import os
 from multiprocessing import Pool
 from time import sleep
+from functools import partial
+from os import path
 
 import requests
 
+def get_data_with_retry(url, max_retries=5):
+    for i in range(max_retries):
+        get = requests.get(url)
+        text = get.text
+        if text is not None and 'timeout' not in text:
+            return get
+        sleep(1)
 
-def download_course(i: int):
+
+def download_course(override_existing_files: bool, i: int):
+    html_path = f'course_details_html/{i}.html'
+    if not override_existing_files and path.exists(html_path):
+        return
+
     url = f'http://moon.cc.huji.ac.il/nano/pages/wfrCourse.aspx?' \
           f'faculty=2&' \
           f'year=2021&' \
           f'courseId={i}'
 
-    with open(f'course_details_html/{i}.html', 'wb') as f:
-        f.write(requests.get(url).content)
+    with open(html_path, 'wb') as f:
+        f.write(get_data_with_retry(url, 5).content)
     print(i)
 
 
-def download_track(i: int):
+def download_track(override_existing_files: bool, i: int):
+    html_path = f'tracks_html/{i}.html'
+    if not override_existing_files and path.exists(html_path):
+        print(i)
+        return
+
     url = f'http://moon.cc.huji.ac.il/nano/pages/wfrMaslulDetails.aspx' \
           f'?year=2021&' \
           f'faculty=2&' \
           f'maslulId=2{i}'
 
-    text = None
-    get = None
-
-    while text is None or 'timeout' in text:  # todo use requests retry instead
-        get = requests.get(url)
-        text = get.text
-        if 'timeout' in text:
-            print(f'({i})')
-            sleep(5)
-
-    with open(f'tracks_html/{i}.html', 'wb') as f:
-        f.write(get.content)
+    with open(html_path, 'wb') as f:
+        f.write(get_data_with_retry(url, max_retries=5).content)
     print(i)
 
 
-def download_all_courses():
+def download_all_courses(override_existing_files=False):
     folder = 'course_details_html'
     if not os.path.exists(folder):
         os.mkdir(folder)
 
     with Pool() as pool:
-        pool.map(download_course, (i for i in range(1_000, 100_000)))
+        func = partial(download_course, override_existing_files)
+        pool.map(func, (i for i in range(1_000, 100_000)))
 
 
-def download_all_tracks():
+def download_all_tracks(override_existing_files=False):
     folder = 'tracks_html'
     if not os.path.exists(folder):
         os.mkdir(folder)
 
     with Pool() as pool:
-        pool.map(download_track, (i for i in range(1_000, 10_000)))
+        func = partial(download_track, override_existing_files)
+        pool.map(func, (i for i in range(1_000, 10_000)))
 
 
 def download_all():
