6165fe892675e1a85de082149412fbd217269def
Daniel Kerbel <NMDanny@gmail.com>
2021-06-20 14:49:41 +0300

    Add axios instance tied to API and auth
    This instance is accessible via `this.$http` within a vue context, and
    has a base URL set to VUE_APP_API_URL.
    
    Moreover, whenever the authentication state changes,
    so does the authorization header on that instance.
    
    In addition, this instance is also accessible via "window.closureAxios"
    for debugging purposes.

--- Closure_Front_End/vue.config.js
+++ Closure_Front_End/vue.config.js
@@ -7,6 +7,9 @@ module.exports = {
     : '/'
 }
 
+// URL must not have a trailing slash. 
+// It may be empty if the API lives in the same domain as 
+// the front-end server.
 process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://closure-service-y46le24bra-ew.a.run.app/'
-  : 'http://localhost:8000/'
+  ? 'https://closure-service-y46le24bra-ew.a.run.app/api/v1'
+  : 'http://localhost:8000/api/v1'
