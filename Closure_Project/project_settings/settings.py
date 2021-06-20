05ee6a9033634084a2f3df9b897d130fbb5b5443
Daniel Kerbel <NMDanny@gmail.com>
2021-06-20 16:02:20 +0300

    No longer require admin permission by default
    This was too restrictive and prevented even browsing the API

--- Closure_Project/project_settings/settings.py
+++ Closure_Project/project_settings/settings.py
@@ -72,9 +72,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',
-    ),
+    'DEFAULT_PERMISSION_CLASSES': ()
 }
 
 AUTHENTICATION_BACKENDS = [
