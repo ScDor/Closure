ef3a283d70a9db485fa842d77475bf9384b52e40
Daniel Kerbel <NMDanny@gmail.com>
2021-06-20 21:22:07 +0300

    Configure frontend via .env

--- Closure_Front_End/vue.config.js
+++ Closure_Front_End/vue.config.js
@@ -2,14 +2,5 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {  
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/closuretest.duckdns.org/'
-    : '/'
+  publicPath: process.env.PUBLIC_PATH
 }
-
-// URL must not have a trailing slash. 
-// It may be empty if the API lives in the same domain as 
-// the front-end server.
-process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://closure-service-y46le24bra-ew.a.run.app/api/v1'
-  : 'http://localhost:8000/api/v1'
