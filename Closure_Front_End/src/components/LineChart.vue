33e76e7531a36ca4563cc5ff0a87f2c63e7b44b4
roey <rbenjos@gmail.com>
2021-06-26 17:22:30 +0300

    added mock analytics

---
+++ Closure_Front_End/src/components/LineChart.vue
@@ -0,0 +1,25 @@
+
+
+<script>
+import { defineComponent } from 'vue';
+import { Line } from 'vue3-chart-v2';
+
+
+export default defineComponent({
+  name: 'MonthlyChart',
+  extends: Line,
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      required: false
+    },
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.chartOptions)
+  }
+})
+</script>
---
+++ Closure_Front_End/src/views/Analytics.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div class="columns">
+  <div class="column">
+    <BarChart/>
+  </div>
+  <div class="column">
+    <LineChart/>
+  </div>
+  <div class="column">
+  </div>
+  <div class="column">
+  </div>
+</div>
+
+
+</template>
+
+<script>
+import BarChart from "../components/BarChart.vue";
+import LineChart from "../components/LineChart.vue";
+
+export default {
+  name: "Analytics",
+  components: { BarChart,LineChart },
+  data() {},
+
+  methods: {},
+};
+</script>
+
+<style>
+
+</style>
