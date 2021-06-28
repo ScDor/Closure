9fd0684b47a1a977f7bb3f9d1ae718c6acbe9aac
Daniel Kerbel <NMDanny@gmail.com>
2021-06-28 03:00:35 +0300

    course store now autosaves to localstorage

--- Closure_Front_End/src/course-store.js
+++ Closure_Front_End/src/course-store.js
@@ -1,10 +1,15 @@
-import { reactive , shallowReadonly } from 'vue'
+import { reactive , shallowReadonly, watch } from 'vue'
 import { default as INITIAL_COURSES }  from './test-courses.js'
 
 
+let lsCourses = localStorage.getItem("courses")
+let initialCourses = INITIAL_COURSES
+if (lsCourses) {
+    initialCourses = JSON.parse(lsCourses)
+}
 
 const state = {
-    courses: reactive(INITIAL_COURSES)
+    courses: reactive(initialCourses)
 }
 
 
@@ -35,5 +40,13 @@ export const deleteCourse = (course) => {
     state.courses.splice(index, 1)
 }
 
+export const addCourses = (courses) => {
+    state.courses.splice(0, state.courses.length)
+    state.courses.push(...courses)
+}
+
+watch(state.courses, newCourses => {
+    localStorage.setItem("courses", JSON.stringify(newCourses))
+})
 
 export const courses = shallowReadonly(state.courses)
