1a016fa5a9f01bea61db85e8414e6ad6744da700
Yael Mualem <yael.mualem@mail.huji.ac.il>
2021-06-29 17:55:33 +0300

    provide default error message if state isn't defined yet

--- Closure_Front_End/src/components/ErrorNotification.vue
+++ Closure_Front_End/src/components/ErrorNotification.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="notification is-danger" v-if="errorState.errorMessage">
-    <p>{{errorState.errorMessage}}</p>
-    <button class="delete" v-on:click="closeErrorMessage()"></button>
-  </div>
+    <div class="notification is-danger" v-if="errorMessage">
+        <p>{{errorMessage}}</p>
+        <button class="delete" v-on:click="closeErrorMessage()"></button>
+    </div>
 </template>
 
 
-
 <script>
-export default {
+    export default {
 
-    inject: ["errorState", "setErrorMessage"],
+        inject: ["errorState", "setErrorMessage"],
 
-    methods: {
-        closeErrorMessage: function () {
-          this.setErrorMessage("");
+        computed:{
+            errorMessage: function () {
+                if (this.errorState){
+                    return this.errorState.errorMessage;
+                }
+                return ''
+            }
+        },
+
+        methods: {
+            closeErrorMessage: function () {
+                this.setErrorMessage("");
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-.notification {
-    position: fixed;
-    top: 0%;
-    left: 32%;
-    display: flex;
-    justify-content:center;
-    min-height: 20vh;
-    min-width: 80vh;
-    z-index: 9999;
-}
+    .notification {
+        position: fixed;
+        top: 0%;
+        left: 32%;
+        display: flex;
+        justify-content: center;
+        min-height: 20vh;
+        min-width: 80vh;
+        z-index: 9999;
+    }
 </style>
 
