cbba7783047d91a1e5a41dd28fe73f704a9923f4
Daniel Kerbel <NMDanny@gmail.com>
2021-08-26 02:49:38 +0300

    add year selection to frontend

--- Closure_Front_End/src/components/Navigation.vue
+++ Closure_Front_End/src/components/Navigation.vue
@@ -1,14 +1,15 @@
   <!-- we will bind every key movement to the searchCourses method so it will update immediately -->
 <template>
+  <year-selection v-model="selectedYear" />
   <search-bar
     :placeholder="'חפש מסלול'"
-    :url="'tracks/?limit=6&offset=15&search='"
+    :url="`tracks/?limit=6&offset=15&data_year=${selectedYear}&search=`"
     @clicksuggestion="emitTrackClick"
   ></search-bar>
 
   <search-bar
     :placeholder="'חפש קורס'"
-    :url="'courses/?limit=6&offset=15&search='"
+    :url="`courses/?limit=6&offset=15&data_year=${selectedYear}&search=`"
     @clicksuggestion="emitCourseClick"
   ></search-bar>
 
@@ -17,11 +18,17 @@
 </template>
 
 <script>
+import YearSelection from './YearSelection.vue'
 import SearchBar from "./SearchBar.vue";
 import ProgressBox from "./ProgressBox.vue";
 
 export default {
-  components: { SearchBar, ProgressBox },
+  components: { YearSelection, SearchBar, ProgressBox },
+  data() {
+    return {
+      "selectedYear": 2022
+    }
+  },
   emits: ["clickcourse"],
   props: ["allcourses"],
 
