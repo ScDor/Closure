c3a1a6980775bf8ec55ced5114b7127d9ca6bf79
Daniel Kerbel <NMDanny@gmail.com>
2021-08-26 22:10:10 +0300

    Simplify FE build
    No longer need two separate build targets

--- Closure_Front_End/vite.config.js
+++ Closure_Front_End/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslint from '@rollup/plugin-eslint'
 import path from 'path'
-import commonConfig from './vite-common.config.js'
 
 const entryFileNameNamingRule = (chunkInfo) => {
   if (chunkInfo.name === "course-scrape-prod") {
@@ -11,7 +12,21 @@ const entryFileNameNamingRule = (chunkInfo) => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  ...commonConfig,
+  plugins: [vue(), { ...eslint({
+    include: '**/*.+(vue|js|jsx|ts|tsx)'
+  }), enforce: 'pre', apply: 'build'}],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    hmr: {
+      protocol: "ws", host: "localhost", port:8080
+    }
+  },
   build: {
     manifest: true,
     sourcemap: true,
