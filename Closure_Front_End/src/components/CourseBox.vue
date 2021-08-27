085c0514021dd0595bf9a02d53f32577327c3458
Daniel Kerbel <NMDanny@gmail.com>
2021-08-27 16:27:45 +0300

    Adjust colors

--- Closure_Front_End/src/components/CourseBox.vue
+++ Closure_Front_End/src/components/CourseBox.vue
@@ -29,10 +29,11 @@ export default {
   methods: {
     colorMatch() {
       if (this.course.type === "MUST") return "must";
+      if (this.course.type === "CHOICE") return "choice";
       if (this.course.type === "CHOOSE_FROM_LIST") return "choose_from_list";
       if (this.course.type === "CORNER_STONE") return "corner_stone";
       if (this.course.type === "SUPPLEMENTARY") return "supplementary";
-      return "choice";
+      return "supplementary";
     },
   },
 };
