4ea153d5aa67600d0be8c7cd5718b9b08a464874
Daniel Kerbel <NMDanny@gmail.com>
2021-08-31 21:57:46 +0300

    Refactor 'Home' to use composition API

--- Closure_Front_End/src/views/Home.vue
+++ Closure_Front_End/src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
-  <div v-if="$auth.isAuthenticated.value">
+  <div v-if="auth.isAuthenticated.value">
     <div dir="rtl">
       <section class="section section-style">
         <div class="columns is-variable is-2">
           <div class="column is-2 is-right is-hidden-mobile is-hidden-touch">
-            <navigation
-              :allcourses="courses"
-            />
+            <navigation :allcourses="courses" />
             <course-detail :course="selectedCourse" v-if="selectedCourse" />
             <progress-box :allcourses="courses" />
           </div>
@@ -25,44 +23,35 @@
 </template>
 
 <script>
+import { ref, inject, provide } from "vue";
 import Year from "../components/Year.vue";
 import Navigation from "../components/Navigation.vue";
-import CourseDetail from '../components/CourseDetail.vue';
+import CourseDetail from "../components/CourseDetail.vue";
 import ProgressBox from "../components/ProgressBox.vue";
-import {
-  courses,
-  moveCourse,
-  deleteCourse,
-  addCourse
-} from "@/course-store.js";
+import { courses } from "@/course-store.js";
+
+const years = [
+  { id: 1, name: "שנה א" },
+  { id: 2, name: "שנה ב" },
+  { id: 3, name: "שנה ג" },
+  { id: 4, name: "שנה ד" },
+];
 
 export default {
   name: "Closure()",
   components: { Year, Navigation, CourseDetail, ProgressBox },
-  data() {
+  setup() {
+    const selectedCourse = ref(null);
+    const selectCourse = (course) => (selectedCourse.value = course);
+    const auth = inject("auth")
+    provide("selectCourse", selectCourse)
     return {
-      years: [
-        { id: 1, name: "שנה א" },
-        { id: 2, name: "שנה ב" },
-        { id: 3, name: "שנה ג" },
-        { id: 4, name: "שנה ד" }
-      ],
+      years,
+      selectedCourse,
       courses,
-      selectedCourse: null
+      auth
     };
   },
-  methods: {
-    moveCourse,
-    deleteCourse,
-    addCourse,
-  },
-  provide() {
-    return {
-      selectCourse: (course) => {
-        this.selectedCourse = course;
-      }
-    };
-  }
 };
 </script>
 
